b79c5d2b819917733044c9364d140e8afa951495
dc <2976237742@qq.com>
2025-10-23 00:50:58 +0800

    feat:01_自己写一个App组件

---
+++ 备份/01_自己写一个App组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- html -->
+    <div class="app">
+        <h1>Hello Vue3 + Vite + TypeScript!</h1>
+    </div>
+</template>
+
+<script lang="ts">
+// js or ts
+    export default {
+        name: 'App' // 组件名称
+    }
+</script>
+
+<style>
+/* 样式 css */
+.app {
+    background-color: #ddd;
+    box-shadow: 0, 0, 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
