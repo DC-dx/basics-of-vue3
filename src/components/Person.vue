9d13b3c68618e2b1b29aad1fabe85559de184d56
DC-dx <29762377@qq.com>
2025-10-26 23:41:13 +0800

    feat:07_ref基本类型响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,6 +2,7 @@
     <div class="person">
         <h2>姓名：{{ name }}</h2>
         <h2>年龄：{{ age }}</h2>
+        <h2>地址：{{ address }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
         <button @click="showTel">查看联系方式</button>
@@ -19,19 +20,26 @@ export default {
 
 <!-- setup语法糖 -->
 <script  lang="ts" name="Person123" setup>
+    import { ref } from 'vue';
     // 数据，原来是写在data函数中，此时的name、age、tel都不是响应式的数据
-    let name = '张三';
-    let age = 18;
+    let name =ref( '张三');
+    let age = ref(18);
     let tel = '13800138000';
+    let address = "广东省深圳市";
+ 
+    console.log(name);
+    console.log(age);
+    console.log(tel);
+    console.log(address);
 
     // 方法
     function changeName() {
-        name = '李四';  //注意：这里修改name变量，并不会更新到视图中
-        console.log(name);  // name变量的值已经被修改，但name不是响应式的，视图不会更新
+        name.value = '李四';  //注意：这里修改name变量，并不会更新到视图中
+        console.log(name.value);  // name变量的值已经被修改，但name不是响应式的，视图不会更新
     }
     function changeAge() {
-        age += 1;   //注意：这里修改age变量，并不会更新到视图中
-        console.log(age);  // age变量的值已经被修改，但age不是响应式的，视图不会更新
+        age.value += 1;   //注意：这里修改age变量，并不会更新到视图中
+        console.log(age.value);  // age变量的值已经被修改，但age不是响应式的，视图不会更新
     }
     function showTel() {
         alert(`联系方式：${tel}`);
