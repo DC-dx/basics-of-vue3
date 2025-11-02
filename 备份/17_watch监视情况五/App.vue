9335ef0a5f4eb3746cdd7a5f284ee850ba2d3de9
DC-dx <29762377@qq.com>
2025-11-02 17:32:13 +0800

    feat:17_监视上述多个数据

---
+++ 备份/17_watch监视情况五/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- html -->
+    <!-- 3、使用Person组件 -->
+    <Person/>
+</template>
+
+<script lang="ts">
+// 为什么不用{}解构赋值？因为Vue组件选项中，必须通过export default导出一个对象，而不是单独导出某些属性或方法
+import Person from './components/Person.vue';   // 1、引入Person组件
+
+
+// js or ts
+export default {
+    name: 'App', // 组件名称
+    components: {
+        Person  // 2、注册Person组件
+    }
+}
+</script>
