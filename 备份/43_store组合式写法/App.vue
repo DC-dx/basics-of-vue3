dd80dff578639ba93bd4fcb4a50147ed89ba6a9a
DC-dx <29762377@qq.com>
2025-11-25 00:11:38 +0800

    feat:43_store组合式写法

---
+++ 备份/43_store组合式写法/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <Count />
+    <br></br>
+    <LoveTalk />
+</template>
+
+<script lang="ts" setup name="App">
+import Count from './Components/Count.vue';
+import LoveTalk from './Components/LoveTalk.vue';
+</script>
---
+++ 备份/43_store组合式写法/store/count.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia';
+export const useCountStore = defineStore('count', {
+    // actions 用于存储方法的地方 用于响应组件中的“动作”
+    actions: {
+        // action存在的意义：可以复用代码  可以封装业务逻辑  可以处理异步操作
+        increment(value: number) {
+            console.log('increment方法被调用了');
+            console.log('value的值为：', value);
+            // this是当前的store实例对象
+            if (this.sum + value > 10) {
+                alert('sum的值不能超过10');
+                return;
+            }
+            this.sum += value;
+        }
+    },
+    // 真正存储数据的地方
+    state() {
+        return {
+            sum: 6,
+            school: 'CSU_csu',
+            address: 'Changsha'
+        }
+    },
+    getters: {
+        // getter用于对state中的数据进行加工处理
+        bigSum: state => state.sum * 10,
+        // 也可以使用this来获取state中的数据
+        upperSchool(): string {
+            console.log('@!!!', this);
+            return this.school.toUpperCase();
+        }
+    }
+});
