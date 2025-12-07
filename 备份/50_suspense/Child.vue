faa1d993a96ac7203352351da57bbcb5ab7cfe6d
DC-dx <29762377@qq.com>
2025-12-08 00:03:45 +0800

    feat:50_suspense

---
+++ 备份/50_suspense/Child.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="child">
+        <h2>我是Child组件</h2>
+        <h3>当前求和为：{{ sum }}</h3>
+        <img v-for="(dog, index) in dogList" :key="index" :src="dog"></img>
+    </div>
+
+</template>
+
+<script setup lang="ts" name="Child">
+import { ref } from 'vue'
+import axios from 'axios';
+let sum = ref(0)
+let dogList = ref<Array<string>>([])
+let result = await axios.get('https://dog.ceo/api/breed/pembroke/images/random');
+dogList.value.push(result.data.message);
+console.log(dogList.value);
+
+</script>
+
+<style scoped>
+.child {
+    background-color: skyblue;
+    border-radius: 10px;
+    padding: 10px;
+    box-shadow: 0 0 10px;
+}
+</style>
